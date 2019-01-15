752fdff1c19f9d04d577dba875e1ebab889acb8c
John Mutuma <john.mutuma@andela.com>
2019-01-15 08:35:30 +0300

    more notes

--- PWAs/app/first-pwa-start/public/serviceWorker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', event => (
-  console.log('[Service Worker] Installing the service worker...', event)
-));
-
-
-self.addEventListener('activate', event => {
-  console.log('[Service Worker] Activating the service worker...', event);
-  return self.clients.claim(); // not compulsory, allows an active service worker to set itself as the controller for all clients
-});
