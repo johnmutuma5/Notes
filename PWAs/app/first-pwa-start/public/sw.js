dc4fa289f75d8d7ebf0b4811c8237a5c0d78d89c
John Mutuma <john.mutuma@andela.com>
2019-01-15 09:23:23 +0300

    [15.01.19] Precaching files

--- PWAs/app/first-pwa-start/public/sw.js
+++ PWAs/app/first-pwa-start/public/sw.js
@@ -1,6 +1,13 @@
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  // cache static files
+  const preCache = async () => {
+    console.log('[Service worker] Precaching app.js')
+    const cache = await caches.open('static-files');
+    await cache.add('/src/js/app.js');
+  }
+  event.waitUntil(preCache());
 });
 
 self.addEventListener('activate', function(event) {
@@ -9,5 +16,11 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+  const getResponse = async () => {
+    const { request } = event;
+    const response = await caches.match(request);
+    return (response && response) || fetch(request);
+  };
+
+  return event.respondWith(getResponse());
 });
